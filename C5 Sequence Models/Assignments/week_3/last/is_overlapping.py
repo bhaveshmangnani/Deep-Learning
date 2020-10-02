666fde59a69759f76e249f6163202f8421d5f9ba
Bhavesh Mangnani <bhaveshmangnani@gmail.com>
2020-10-02 21:56:44 +0530

    manual assignments

---
+++ C5 Sequence Models/Assignments/week_3/last/is_overlapping.py
@@ -0,0 +1,28 @@
+# GRADED FUNCTION: is_overlapping
+
+def is_overlapping(segment_time, previous_segments):
+    """
+    Checks if the time of a segment overlaps with the times of existing segments.
+    
+    Arguments:
+    segment_time -- a tuple of (segment_start, segment_end) for the new segment
+    previous_segments -- a list of tuples of (segment_start, segment_end) for the existing segments
+    
+    Returns:
+    True if the time segment overlaps with any of the existing segments, False otherwise
+    """
+    
+    segment_start, segment_end = segment_time
+    
+    ### START CODE HERE ### (≈ 4 lines)
+    # Step 1: Initialize overlap as a "False" flag. (≈ 1 line)
+    overlap = False
+    
+    # Step 2: loop over the previous_segments start and end times.
+    # Compare start/end times and set the flag to True if there is an overlap (≈ 3 lines)
+    for previous_start, previous_end in previous_segments:
+        if segment_start <= previous_end and segment_end >= previous_start:
+            overlap = True
+    ### END CODE HERE ###
+
+    return overlap
